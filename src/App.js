4a3e553721b4e85285643855bffaf0c4fcf0cb11
jacqueline-solomongit congig --global user.email shavlinshatrakh82@gmail.com <shavlinshatrakh82@gmail.com git config --global user.name jacqueline-solomon>
2023-08-22 21:42:37 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Link }from 'react-scroll'
+
+const menuItems=
+[{
+  id:1,
+  title:"home",
+  content:"Out here, under the open sky and endless stars, you can't buy happiness but you can buy a tickets.",
+
+},
+
+{
+  id:2,
+  title:"About",
+  content:"Enjoy ur life with travel and reach your dream destination."
+},
+
+{
+  id:3,
+  title:"Contact",
+  content:"Happiness is to fullfill our dreams so dream with your lovely places.",
+},
+
+{
+  id:4,
+  title:"Service",
+  content:"you can't buy a time and happiness but you can buy a tickets for ur dream destination soo book a tickets.",
+}
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <h1>ROUTER</h1>
+          <ul>
+            {menuItems.map((menu) => (
+              <li>
+                <Link
+                  to={menu.title}
+                  smooth={true}
+                  offset={-430}
+                  duration={500}
+                  
+                >
+                  {menu.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </header>
+
+
+      <main>
+  {menuItems.map((menu) => (
+    <div className="content" key={menu.id}>
+      <h1 className="content-header" id={menu.title}>
+        
+      </h1>
+      <h3>{menu.content}</h3>
+    </div>
+  ))}
+</main>
+
     </div>
   );
 }
